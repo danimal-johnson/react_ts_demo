c8799e9fc05dd9f090f93111ed6bcc54891d9d2f
Dan Johnson <danimal.johnson@gmail.com>
2020-07-19 23:32:55 -0700

    Add Counter example (SetState example).

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 interface Person {
   firstName: string;
@@ -15,12 +15,15 @@ interface Props {
 
 export const TextField: React.FC<Props> = ({}) => {
   const [count, setCount] = useState<number | null>(5);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
   setCount(12);
+  console.log(count);
 
   return (
-    <div>
-      <input />
+    <div ref={divRef}>
+      <input ref={inputRef} />
     </div>
   );
 };
